634aeeaf4628a74b68fcb4dcaf9878c2f4aafb90
Alan Tai <awtai@ucdavis.edu>
2015-06-22 13:21:11 -0700

    Blog entry dotted line changed
    Experimenting with different styles. Don't want too many different
    colors on the page, so all colors are more similar. Dotted line changed
    to dashed line so that there is more spacing between the "dots."

--- travelblog/assets/js/blog_entries.js
+++ travelblog/assets/js/blog_entries.js
@@ -3,7 +3,8 @@
 * All function calls should follow the following format:
 *   <img src="../images/loaded.gif" alt=""
      onload="run_function();this.parentNode.removeChild(this);" />
+* blank space: &emsp;
 */
 var blog_entries = [
-  "<h1>June 21st, 2015</h1>Created a blog to record my travels. I will start my adventure in China and go to Hong Kong, Macao, and Taiwan. I'll try to keep this as up-to-date as possible, so please drop by and check it out once in a while! I will occasionally post a status update on Facebook whenever there is more stuff! I can't wait to travel and I can't wait to share it with you all. :)<br><br><strong>And don't worry guys. There will definitely be pictures!</strong>"
+  "<h1>June 21st, 2015</h1>Created a blog to record my travels. I will start my adventure in China and go to Hong Kong, Macao, and Taiwan. I'll try to keep this as up-to-date as possible, so please drop by and check it out once in a while! I will occasionally post a status update on Facebook whenever there is more stuff! I can't wait to travel and I can't wait to share it with you all. :)<br><br>&emsp;<strong>And don't worry guys, there will definitely be pictures!</strong>"
   ];
